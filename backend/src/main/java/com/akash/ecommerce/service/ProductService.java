8399d1e6d3f6a2990441c6fda3331c93adf08972
Akash Sharma <akash.sharma@arrise.com>
2026-07-17 11:29:53 +0530

    Handled null values

--- backend/src/main/java/com/akash/ecommerce/service/ProductService.java
+++ backend/src/main/java/com/akash/ecommerce/service/ProductService.java
@@ -18,6 +18,6 @@ public class ProductService {
     }
 
     public Product getProductById(int id) {
-        return repo.findById(id).orElse(new Product());
+        return repo.findById(id).orElse(null);
     }
 }
